8d20a90bc90b490b95283e8fa7d911384288eb18
奋仔 <felicitychou@hotmail.com>
2016-07-13 18:15:32 +0800

    upload ssh
    upload ssh

---
+++ Black Hat Python/Chapter 2/bh_sshcmd.py
@@ -0,0 +1,16 @@
+import threading
+import paramiko
+import subprocess
+
+def ssh_command(ip, username, password, command):
+	client = paramiko.SSHClient()
+	#client.load_host_keys('/home/felicitychou/.ssh/known_hosts')
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	client.connect(ip, username=username, password=password)
+	ssh_session = client.get_transport().open_session()
+	if ssh_session.active:
+		ssh_session.exec_command(command)
+		print ssh_session.recv(1024)
+	return
+
+ssh_command('192.168.100.131'.'felicitychou','password','id')
