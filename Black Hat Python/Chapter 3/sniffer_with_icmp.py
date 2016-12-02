d7cca9dcd9fe83a61bcf2272140770bd6f8164a4
fen <felicitychou@hotmail.com>
2016-12-02 15:37:10 +0800

    update chapter 3

--- Black Hat Python/Chapter 3/sniffer_with_icmp.py
+++ Black Hat Python/Chapter 3/sniffer_with_icmp.py
@@ -35,6 +35,21 @@ class IP(Structure):
         except:
             self.protocol = str(self.protocol_num)
 
+class ICMP(Structure):
+    _fields_ = [
+    ("type", c_ubyte),
+    ("code", c_ubyte),
+    ("checksum", c_ushort),
+    ("unused", c_ushort),
+    ("next_hop_mtu", c_ushort)
+    ]
+
+    def __new__(self, socket_buffer):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer):
+        pass
+
 
 if os.name == "nt":
     socket_protocol = socket.IPPROTO_IP
@@ -59,9 +74,16 @@ try:
 
         print "Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address)
 
+        if ip_header.protocol == "ICMP":
+
+            offset = ip_header.ihl * 4
+            buf = raw_buffer[offset:offset + sizeof(ICMP)]
+
+            icmp_header = ICMP(buf)
+
+            print "ICMP -> Type: %d Code: %d" % (icmp_header.type, icmp_header.code)
+
 except KeyboardInterrupt:
 
     if os.name == "nt":
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
-
-
